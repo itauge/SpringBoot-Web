12ae39f0b2080aacce49ccb4bd31fedb367fd5b7
itauge <itaugee@gmail.com>
2021-06-13 22:57:05 +0800

    commit

---
+++ src/main/java/com/example/springbootweb/config/WebConfig.java
@@ -0,0 +1,43 @@
+package com.example.springbootweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
+import sun.plugin2.message.helper.URLHelper;
+
+/*
+* 設置Filter轉換引用Delete/put方法的名
+* */
+@Configuration(proxyBeanMethods = false)
+public class WebConfig /*implements WebMvcConfigurer*/ {
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        HiddenHttpMethodFilter methodFilter = new HiddenHttpMethodFilter();
+        methodFilter.setMethodParam("_m");
+        return methodFilter;
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void configurePathMatch(PathMatchConfigurer configurer) {
+                UrlPathHelper urlPathHelper = new UrlPathHelper();
+                urlPathHelper.setRemoveSemicolonContent(false);
+                configurer.setUrlPathHelper(urlPathHelper);
+            }
+        };
+
+    }
+
+//    @Override
+//    public void configurePathMatch(PathMatchConfigurer configurer){
+//        UrlPathHelper urlPathHelper = new UrlPathHelper();
+//        urlPathHelper.setRemoveSemicolonContent(false);
+//        configurer.setUrlPathHelper(urlPathHelper);
+//    }
+}
---
+++ src/main/java/com/example/springbootweb/controller/RequestController.java
@@ -0,0 +1,33 @@
+package com.example.springbootweb.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class RequestController {
+
+    @GetMapping("/goto")
+    public String goToPage(HttpServletRequest request){
+        request.setAttribute("msg","success..");
+        request.setAttribute("code",200);
+        return "forward:/success"; //轉發到 /success請求
+    }
+
+    @ResponseBody
+    @RequestMapping("/success")
+    public Map success(@RequestAttribute("msg") String msg,
+                          @RequestAttribute("code") Integer code,
+                          HttpServletRequest request){
+        Object msg1 = request.getAttribute("msg");
+        Map<String,Object> map = new HashMap<>();
+        map.put("request_msg",msg1);
+        map.put("annotation_msg",msg);
+        return map;
+    }
+}
